073f930c06be5a303edbdbae883324e28aea6b84
KI\25110 <25110@edu.amw.gdynia.pl>
2025-03-13 11:20:05 +0100

    no message

--- simple-programming-tasks/skrypt22-25110.py
+++ simple-programming-tasks/skrypt22-25110.py
@@ -4,3 +4,19 @@
 
 def szukanie_najdluzszy():
     with open("wordlist_10000.txt", 'r') as plik:
+        naj_dlug = 0
+        for numer_linii, linia in enumerate(plik, start=1):
+            if (len(linia)>naj_dlug):
+                naj_dlug=len(linia)
+        return naj_dlug
+
+def szukanie_dlug(dlug):
+    with open("wordlist_10000.txt", 'r') as plik:
+        liczba = 0
+        for numer_linii, linia in enumerate(plik, start=1):
+            if (len(linia)==dlug):
+                liczba+=1
+        return liczba
+
+print(str(szukanie_najdluzszy()))
+print(str(szukanie_dlug(10)))
---
+++ simple-programming-tasks/skrypt23-25110.py
@@ -0,0 +1,9 @@
+#Za pomocą pętli stworzyć 1000 losowych 6 znakowych wyrazów
+#  [A-Z][a-z][0-9] i zapisać je do pliku passwords.txt.
+import random
+import string
+characters = string.ascii_uppercase + string.ascii_lowercase + string.digits
+slowa = []
+for x in range(1000):
+    slowa.append([random.choice(characters) for x in range(6)])
+print(slowa)
